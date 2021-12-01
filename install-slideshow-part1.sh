15a6d965e3610fbec172765afe2b4f4051549d04
Jason Bramwell <jb2cool@gmail.com>
2021-12-01 23:47:27 +0000

    Update and rename install-slideshow.sh to install-slideshow-part1.sh

--- install-slideshow-part1.sh
+++ install-slideshow-part1.sh
@@ -26,16 +26,3 @@ sudo wget --no-check-certificate https://raw.githubusercontent.com/jb2cool/slide
 
 # Reboot (to apply new mount settings)
 sudo reboot
-
-# Copy .xsession to /home/pi - assuming pi is the user id setup to run on bootup
-# This script starts the display manager and syncs the default user home with a fresh copy from /media/usb/pi
-# It also deletes any previous LibreOffice config allowing clean start of slideshow
-wget --no-check-certificate https://raw.github.com/jb2cool/slideshow/master/.xsession
-
-# Insert the USB transfer stick (should have the label usb or you'll need to change some stuff)   
-
-# Create a copy of the default user (pi) in /media/usb
-sudo cp -r /home/pi /media/usb/
-cd /media/usb/pi/
-sudo chmod -R a+r .
-sudo chmod a+x .xsession
