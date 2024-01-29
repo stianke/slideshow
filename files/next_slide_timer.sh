796eed0e8dada56fe9beff681d1f410774c212ac
StianKristoffer <stianke@stud.ntnu.no>
2024-01-29 17:12:57 +0100

    Replace spaces with tabs in next_slide_timer.sh

--- files/next_slide_timer.sh
+++ files/next_slide_timer.sh
@@ -8,13 +8,13 @@ mkdir tmp
 cp -r $usb_path/*.txt tmp
 if test -f "$usb_path/seconds.txt"; then
         seconds=$(cat $usb_path/seconds.txt)
-        echo $seconds > seconds.txt
+	echo $seconds > seconds.txt
 elif [ "$(ls -A tmp)" ]; then
         files=(tmp/*txt)
         seconds=$(cat "${files[0]}")
-        echo $seconds > seconds.txt
+	echo $seconds > seconds.txt
 elif test -f seconds.txt; then
-        seconds=$(cat seconds.txt)
+	seconds=$(cat seconds.txt)
 else
         seconds=20
 fi
